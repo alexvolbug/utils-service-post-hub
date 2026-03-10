741253c8ac4052fa95325d5140d7f635f5beab1f
alex20x20 <alex20x20@gmail.com>
2026-03-11 00:23:53 +0300

    fix swagger proxy headers and remove hardcoded openapi servers

--- src/main/java/com/post_hub/utils_service/config/OpenApiConfig.java
+++ src/main/java/com/post_hub/utils_service/config/OpenApiConfig.java
@@ -6,17 +6,10 @@ import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springdoc.core.models.GroupedOpenApi;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 @Configuration
 @OpenAPIDefinition(
@@ -42,32 +35,11 @@ import java.util.Objects;
 )
 public class OpenApiConfig {
 
-    @Value("${swagger.servers.first:#{null}}")
-    private String firstServer;
-
-    @Value("${swagger.servers.second:#{null}}")
-    private String secondServer;
-
     @Bean
     public GroupedOpenApi utilsApi() {
         return GroupedOpenApi.builder()
                 .group("utils-service")
                 .packagesToScan("com.post_hub.utils_service.controller")
-                .addOpenApiCustomizer(serverCustomizer())
                 .build();
-    }
-
-    @Bean
-    public OpenApiCustomizer serverCustomizer() {
-        return openApi -> {
-            List<Server> servers = new ArrayList<>();
-            if (Objects.nonNull(firstServer)) {
-                servers.add(new Server().url(firstServer).description("API Server"));
-            }
-            if (Objects.nonNull(secondServer)) {
-                servers.add(new Server().url(secondServer).description("Second API Server"));
-            }
-            openApi.setServers(servers);
-        };
     }
 }
